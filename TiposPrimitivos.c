ce0a266f895e7004b0eef592c4220ba56413b65c
Renato-Barreto <143633174+Renato-Barreto@users.noreply.github.com>
2023-09-25 14:42:06 -0300

    Add files via upload

---
+++ TiposPrimitivos.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(){
+    //declarando variavéis
+    int idade = 18; //Inteiro
+
+    //exibindo dados para o usuário
+    printf("Idade: %d anos", idade);
+
+    return 0;
+}
