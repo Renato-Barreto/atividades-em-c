ccea600d88092ebdbd9c45d597847c05e600c58a
Renato-Barreto <143633174+Renato-Barreto@users.noreply.github.com>
2023-09-25 15:34:00 -0300

    Update DadosDoUsuario.c

--- DadosDoUsuario.c
+++ DadosDoUsuario.c
@@ -4,16 +4,21 @@
 int main() {
     //declarando variáveis
     int idade;
+    float peso;
 
     //solicitando dados do usuário
     printf("Digite sua idade: ");
     scanf("%d",&idade);
 
+    printf("Digite seu peso: ");
+    scanf("%f",&peso);
+
     //LimpaTela (Limpar terminal)
     system("cls || clear");
 
     //exibindo dados do usuário.
-    printf("Idade: %d", idade);
+    printf("Idade: %d \n", idade);
+    printf("Peso: %.2f", peso);
 
     return 0;
 }
