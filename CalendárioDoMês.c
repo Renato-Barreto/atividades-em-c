3e881418d6a031242396425aad364152627dbfb1
Renato-Barreto <143633174+Renato-Barreto@users.noreply.github.com>
2023-10-04 16:51:24 -0300

    Add files via upload

---
+++ CalendárioDoMês.c
@@ -0,0 +1,67 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <locale.h>
+#include <string.h>
+
+int main()
+{
+    setlocale(LC_ALL, "portuguese");
+
+    // Definindo variáveis
+    int dia;
+
+    // Solicitando ao usuário
+
+    printf("Digite o número do mês:");
+    scanf("%i", &dia);
+
+    system("cls || clear"); // Limpa Terminal
+
+    // Exibindo dados com Switch Case
+
+    switch (dia)
+    {
+    case 1:
+        printf("Janeiro");
+        break;
+    case 2:
+        printf("Fevereiro");
+        break;
+    case 3:
+        printf("Março");
+        break;
+    case 4:
+        printf("Abril");
+        break;
+    case 5:
+        printf("Maio");
+        break;
+    case 6:
+        printf("Junho");
+        break;
+    case 7:
+        printf("Julho");
+        break;
+    case 8:
+        printf("Agosto");
+        break;
+    case 9:
+        printf("Setembro");
+        break;
+    case 10:
+        printf("Outubro");
+        break;
+    case 11:
+        printf("Novembro");
+        break;
+    case 12:
+        printf("dezembro");
+        break;
+
+    default:
+        printf("código inválido");
+        break;
+    }
+
+    return 0;
+}
