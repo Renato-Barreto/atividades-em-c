196798aaccbaa98e8b38fa4c33b226d942a1cd87
Renato-Barreto <143633174+Renato-Barreto@users.noreply.github.com>
2023-10-09 13:43:58 -0300

    Update Mercado.c

--- Mercado.c
+++ Mercado.c
@@ -7,21 +7,23 @@ int main()
 {
     setlocale(LC_ALL, "portuguese");
 
-    // Definindo Váriaveis
+    // Definindo VÃƒÂ¡riaveis
     float valorDoProduto;
+    float valorPorParcelas;
     float valorFinal;
     float desconto;
     int quantidadeDeParcelas;
-    int valorPorParcelas;
     char formaDePagamento;
 
-    // Solicitando dados do usuário
+    // Solicitando dados do usuÃƒÂ¡rio
 
     printf("Digite o valor do produto que deseja comprar:");
     scanf("%f", &valorDoProduto);
 
+    fflush(stdin);
+
     printf("Formas de Pagamento \n");
-    printf("(V) - Á vista \n");
+    printf("(V) - Ã vista \n");
     printf("(P) - Parcelado \n");
     printf("Digite a forma de pagamento:\n");
     scanf("%c", &formaDePagamento);
@@ -30,36 +32,40 @@ int main()
 
     switch (formaDePagamento)
     {
-    case "V":
+    case 'V':
         desconto = valorDoProduto * 0.10;
         valorFinal = (valorDoProduto - desconto);
 
         printf("Valor do Produto: %.1f \n", valorDoProduto);
-        printf("Forma de Pagamento: Á Vista \n");
+        printf("Forma de Pagamento: Ã Vista \n");
         printf("Valor do Desconto: %.1f \n", desconto);
-        printf("Total a Pagar: %f", valorFinal);
-        break;
-    
-    case "P":
-        printf("Digite a quantidade de parcelas:");
-        scanf("%i", &quantidadeDeParcelas);
+        printf("Total a Pagar: %.1f", valorFinal);
         break;
 
-        if (parcelas > 6) {
-         printf("Número de parcelas excedido");
-        }else{
-         valorPorParcela = (valorDoProduto / quantidadeDeParcelas);
-         printf("Valor do Produto: %.1f \n", valorDoProduto);
-         printf("Forma de Pagamento: PARCELADO \n");
-         printf("Quantidade de parcelas: %i \n",quantidadeDeParcelas);
-         printf("Valor das parcelas: %i \n", valorPorParcela);
-         printf("Total a Pagar: %.1f", valorFinal);
-         break;
+    case 'P':
+        printf("Digite a quantidade de parcelas:");
+        scanf("%i", &quantidadeDeParcelas);
+
+        if (quantidadeDeParcelas <= 6)
+        {
+            valorPorParcelas = (valorDoProduto / quantidadeDeParcelas);
+            valorFinal = valorDoProduto;
+
+            printf("Valor do Produto: %.1f \n", valorDoProduto);
+            printf("Forma de Pagamento: PARCELADO \n");
+            printf("Quantidade de parcelas: %i \n", quantidadeDeParcelas);
+            printf("Valor das parcelas: %.1f \n", valorPorParcelas);
+            printf("Total a Pagar: %.1f", valorFinal);
         }
-    
+        else
+        {
+            printf("NÃºmero de parcelas excedido");
+        }
+        break;
     default:
-        printf("Forma de Pagamento Inválida");
+        printf("Forma de Pagamento InvÃ¡lida");
         break;
     }
 
-    
+    return 0;
+}
