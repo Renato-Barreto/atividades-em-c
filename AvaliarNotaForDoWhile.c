cb0b73c78d71606debd57d71be4e421855f1d961
Renato-Barreto <143633174+Renato-Barreto@users.noreply.github.com>
2023-10-11 16:31:08 -0300

    Add files via upload

---
+++ AvaliarNotaForDoWhile.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <locale.h>
+#include <string.h>
+#include <ctype.h>
+
+int main()
+{
+    setlocale(LC_ALL, "portuguese");
+
+int i;
+float notas;
+float soma;
+float media;
+
+
+for ( i = 1; i <= 2; i++)
+{
+    do
+    {
+    printf("Informe a %i° nota: ",i);
+    scanf("%f", &notas);
+
+    if (notas < 0 || notas > 10)
+    {
+    printf("Nota inválida. \nDigite a nota novamente... \n\n");
+    }
+    } while (notas < 0 || notas > 10);
+
+    soma += notas;
+    
+}
+
+media = soma / 2;
+
+printf("Sua média: %.2f ", media);
+
+
+
+return 0;
+}
