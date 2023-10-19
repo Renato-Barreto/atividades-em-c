aaf052992735aafca7ddea7914019b8a6cd2125a
Renato-Barreto <143633174+Renato-Barreto@users.noreply.github.com>
2023-10-19 16:42:48 -0300

    Add files via upload

--- LoginSenha.c
+++ LoginSenha.c
@@ -1,33 +1,47 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
 #include <locale.h>
+#include <string.h>
+#include <ctype.h>
 
 int main()
 {
     setlocale(LC_ALL, "portuguese");
 
-	//declarando variÃ¡veis
-    char loginSalvo [250] = "shunli";
-    char senhaSalva [250] = "filaboia";
-    char login [250];
-    char senha [250];
-
-	//solicitando dados
-    printf("Digite o login: ");
-    scanf("%s",&login);
+    // Declarando Varáveis
+    char loginSalvo[250] = "RenatoBarreto";
+    char login[250];
+    char senhaSalva[250] = "XaolinPeixaria";
+    char senha[250];
     
-	printf("Digite a senha: ");
-    scanf("%s",&senha);  
-    
-    //comparando = strcmp = string comparasion
+    // Solicitando Dados
 
-    if (strcmp(login, loginSalvo) == 0 && strcmp(senha, senhaSalva) == 0) {
-        printf("Welcome");
+    do{
+    printf("Bem-Vindo ao SENAI ADS\n");
+    printf("Iniciaremos com a solicitação dos dados do login\n");
+    printf("\n");
+    printf("Digite o login:");
+    gets(login);
+
+    printf("Digite a senha:");
+    gets(senha);
+
+    system("cls||clear");
+
+    //Avaliando Dados
+
+    if (strcmp(login, loginSalvo) == 0 && strcmp(senha, senhaSalva) == 0)
+    {
+        printf("Login efetuado com sucesso!\n");
+        printf("Bem-Vindo ao Site");
     } else {
-        printf("Not Welcome");
+        printf("Dados incorretos\n");
+        printf("Digite seus dados novamente...\n");
+        printf("\n");
     }
+    
+    
+    } while (strcmp(login, loginSalvo) != 0 && strcmp(senha, senhaSalva) != 0);
 
-return 0;
-
+    return 0;
 }
