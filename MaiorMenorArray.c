f59c9693241f7c239898654a38f4085d926434b5
Renato-Barreto <143633174+Renato-Barreto@users.noreply.github.com>
2023-11-09 16:26:09 -0300

    Update MaiorMenorArray.c

--- MaiorMenorArray.c
+++ MaiorMenorArray.c
@@ -7,7 +7,7 @@
 int main () {
     setlocale(LC_ALL,"portuguese");
 
-     //Declarando Vari·veis
+     //Declarando Vari√°veis
     int i;
     int tamanho = 5;
     int numero[tamanho];
@@ -18,25 +18,17 @@ int main () {
     printf("Digite os valores inteiros \n");
     for(i = 0; i < tamanho; i++) 
     {
-    printf("%i∫ n˙mero: ",i+1);
-        scanf("%i", &numero[i]); 
-    }   
+    printf("%i¬∫ n√∫mero: ",i+1);
+    scanf("%i", &numero[i]); 
 
     //Calculando
-    maior = numero[0] > numero[1] ? numero[0] : numero[1];
-    maior = maior > numero[2] ? maior : numero[2];
-    maior = maior > numero[3] ? maior : numero[3];
-    maior = maior > numero[4] ? maior : numero[4];
+    maior = numero[0] > numero[i] ? numero[0] : numero[i];
+    menor = numero[0] < numero[i] ? numero[0] : numero[i];
+    }   
 
-    menor = numero[0] < numero[1] ? numero[0] : numero[1];
-    menor = menor < numero[2] ? menor : numero[2];
-    menor = menor < numero[3] ? menor : numero[3];
-    menor = menor < numero[4] ? menor : numero[4];
-    
     //Mostrando Dados
-
-    printf("\nO maior n˙mero È: %i \n", maior);
-    printf("O menor n˙mero È: %i", menor);
+    printf("\nO maior n√∫mero √©: %i \n", maior);
+    printf("O menor n√∫mero √©: %i", menor);
     
     return 0;
 }
