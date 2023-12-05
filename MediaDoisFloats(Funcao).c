94f6757f0f034f5e6c9758419deac68fc7b8080a
Renato-Barreto <143633174+Renato-Barreto@users.noreply.github.com>
2023-12-05 15:08:56 -0300

    Update MediaDoisFloats(Funcao).c

--- MediaDoisFloats(Funcao).c
+++ MediaDoisFloats(Funcao).c
@@ -4,10 +4,24 @@
 #include <string.h>
 #include <time.h>
 
-float mediaFuncao (int n1, int n2){
+float mediaFuncao (float n1, float n2)
+{
     float resultado;
     resultado = (n1 + n2) / 2;
     return resultado;
+}
+void contagem()
+{
+    int a;
+    for ( a = 0; a < 3; a++)
+    {
+        printf(".");
+        sleep(1);
+    } 
+}
+void limpaTela()
+{
+    system("cls || clear");
 }
 
 int main(){
@@ -20,15 +34,17 @@ int main(){
 
     printf("Escreva o primeiro numero: ");
     scanf("%f",&primeiroNumero);
+    limpaTela();
 
     printf("Escreva o segundo numero: ");
     scanf("%f",&segundoNumero);
+    limpaTela();
+    contagem();
+    limpaTela();
 
     media = mediaFuncao(primeiroNumero, segundoNumero);
 
     printf("Media: %.1f",media);
-
-
 
     return 0;
 }
